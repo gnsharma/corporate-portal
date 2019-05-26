cb981e68684b84d0c10be738ff808f9a86644010
Govind Sharma <gnsharma0810@gmail.com>
2019-05-26 09:06:50 +0530

    api

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,30 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Cantilever</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+
+    <v-content>
+      <test-table />
+    </v-content>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+import TestTable from '@/components/TestTable.vue'; // @ is an alias to /src
+
+export default {
+  name: 'App',
+  components: {
+    TestTable
+  },
+  data () {
+    return {
+      //
     }
   }
 }
-</style>
+</script>
